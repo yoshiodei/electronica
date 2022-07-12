508a39b57330a434d93647b5d236b2662975b950
yoshiodei <yoshiodei@yahoo.com>
2022-07-12 00:53:12 +0000

    create MainPage component

---
+++ src/pages/mainPage/MainPage.jsx
@@ -0,0 +1,12 @@
+import Navbar from "../../navigation/Navbar";
+
+const MainPage = () => {
+    return (
+        <main>
+            <Navbar />
+            <div>Home</div>
+        </main>
+    );
+}
+
+export default MainPage;
